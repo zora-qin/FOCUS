ab9aed87eb7ec5608792a7eb7759bbb683e154f9
zora-qin <zorazhenqin@outlook.com>
2019-12-03 16:53:27 -0600

    study mode finished 12/3

--- app/src/main/java/com/eshanazora/focus/MainActivity.java
+++ app/src/main/java/com/eshanazora/focus/MainActivity.java
@@ -6,7 +6,14 @@ import android.os.CountDownTimer;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,11 +22,14 @@ import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
+
 public class MainActivity extends AppCompatActivity {
     //main activity;
     private Button studyMode;
     private Button goalsMode;
     private Button motivationMode;
+    private TextView quote;
+    public static String INTERNET;
 
     private static final String study = "Study for an hour.";
     @Override
@@ -52,10 +62,34 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         motivationMode = findViewById(R.id.quote);
+        final RequestQueue queue = Volley.newRequestQueue(this);
+        final String url ="https://quote-garden.herokuapp.com/quotes/random";
+
         motivationMode.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 motivationActivity();
+                //send a request
+               quote = findViewById(R.id.quote);
+                // Instantiate the RequestQueue.
+// Request a string response from the provided URL.
+                StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                        new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String response) {
+                                // Display the first 500 characters of the response string.
+                                quote.setText("Response is: "+ response.substring(0,500));
+                            }
+                        }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        quote.setText("That didn't work!");
+                    }
+                });
+
+// Add the request to the RequestQueue.
+                queue.add(stringRequest);
+
             }
         });
     }
